681e65ba2f5792b721b6fdd8b472fcb24a5c46e7
Vinod Pal <vinod.20.pal@gmail.com>
2024-12-17 19:53:20 +0530

    integrate app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+import { JwtModuleModule } from './jwt-module/jwt-module.module';
+import { DatabaseModule } from './provider/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { DocumentManagementModule } from './document_management/document_management.module';
+import { MulterModuleModule } from './multer-module/multer-module.module';
 
+@Global()
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule, 
+    DatabaseModule, 
+    UserModule, 
+    DocumentManagementModule, 
+    MulterModuleModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
